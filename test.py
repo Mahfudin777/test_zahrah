fabb0f8a97c173fa0c7c9c910fbc7cdeb461f588
Mahfudin777 <mahfudin@students.esqbs.ac.id>
2024-12-27 19:25:21 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from ultralytics import solutions
 
-# Pass a model as an argument
-solutions.inference(model="best.pt")
+inf = solutions.Inference(
+    model="best.pt",  # You can use any model that Ultralytics support, i.e. YOLO11, or custom trained model
+)
 
-### Make sure to run the file using command `streamlit run <file-name.py>`
+inf.inference()
+
+### Make sure to run the file using command `streamlit run <file-name.py>
